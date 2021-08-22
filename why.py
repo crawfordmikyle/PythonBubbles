d619f85be6fd619863722f1019dbb5e1db762210
Mikyle Crawford <mikylec@localhost.localdomain>
2021-08-22 01:08:41 -0700

    cant use index due to problem caused by two or more of the same number

--- why.py
+++ why.py
@@ -1,6 +1,9 @@
 # why do we use range?
 # why can I not index like JavaScript
 
-nums = [10, 100, 50, 30, 75]
+nums = [10, 100, 50, 30, 75, 10]
 
-print(nums.index(10))
+#print(nums.index(10))
+
+for each in nums:
+	print (nums.index(each))
